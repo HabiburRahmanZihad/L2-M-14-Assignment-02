1c253bcd985108bb8f1e7b71fe960fec0d0753e9
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-12-05 20:27:27 +0600

    fix the code to deploy

--- api/server.ts
+++
@@ -1,5 +0,0 @@
-import serverless from 'serverless-http'
-import app from '../src/app'
-
-// Wrap the Express app with serverless-http and export as the default handler
-export default serverless(app as any)
